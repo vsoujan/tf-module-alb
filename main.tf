61ecde0c71078325a9910c7d11a3a46a3942b93d
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-09-17 00:43:20 +0530

    alb

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ resource "aws_lb" "main" {
 }
 
 resource "aws_security_group" "main" {
-  name        = "${var.env}-alb-sg"
-  description = "${var.env}-alb-sg"
+  name        = local.sg_name
+  description = local.sg_name
   vpc_id      = var.vpc_id
-  tags = merge(local.tags, { Name = "${var.env}-alb-sg" })
+  tags        = merge(local.tags, { Name = local.sg_name })
 
   ingress {
     description = "APP"
